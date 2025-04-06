be05ab42d086c810dd212b230c53c843e322a526
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-06 16:58:32 -0300

    Criando a página de login

---
+++ src/routes/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <section>
+      <h1>Login</h1>
+      <div>
+        
+      </div>
+    </section>
+  )
+}
+
+export default Login
