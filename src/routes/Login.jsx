5042ec6df7401d3bc9fed17a702f84d9f5910a52
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-07 10:28:07 -0300

    Mudando o design no login web e mobile

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import './Login.css'
+import React from 'react';
 import Image from "../assets/Logo.png";
+import './Login.css';
 
 const Login = () => {
   return (
     <section className='login'>
-      <div id='login-title'>
-        <h1>Login</h1>
-      </div>
-
       <div id='login-box'>
-        <img src={Image} alt="" width={100} height={100}/>
-        <input type="text" placeholder='Insira o seu nome'/>
-        <input type="password" placeholder='Insira sua senha'/>
+        <img src={Image} alt="" width={100} height={100} />
+        <div id='login-title'>
+          <h1>Login</h1>
+        </div>
+        <input type="text" placeholder='Insira o seu nome' />
+        <input type="password" placeholder='Insira sua senha' />
         <button>Entrar</button>
       </div>
     </section>
