e13f6fc4143caf3a0b3ba98beb25a2f170edbb3c
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-10 19:59:05 -0300

    Reformando o login com forms

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "../assets/Logo.png";
 import './Login.css';
 
 const Login = () => {
-  console.log('Hello World')
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const userLogin = (e) => {
+    e.preventDefault();
+    
+  }
   return (
     <section className='login'>
       <div id='login-box'>
@@ -11,9 +16,11 @@ const Login = () => {
         <div id='login-title'>
           <h1>Login</h1>
         </div>
-        <input type="text" placeholder='Insira o seu nome' />
-        <input type="password" placeholder='Insira sua senha' />
-        <button>Entrar</button>
+        <form>
+          <input type="text" className='loginInput'placeholder='Insira o seu nome'  required onChange={(c) => setName(c.target.value)}/>
+          <input type="password" className='loginInput' placeholder='Insira sua senha'  required onChange={(c) => setPassword(c.target.value)}/>
+          <button className='loginButton' onClick={userLogin}>Entrar</button>
+        </form>
       </div>
     </section>
   )
