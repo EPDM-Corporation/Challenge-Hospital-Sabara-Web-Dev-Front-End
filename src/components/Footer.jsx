d91a53ea36656e65579f60a8617c85bca08ddae8
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-12 14:50:32 -0300

    Adicionando icones ao footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Footer.css'
+import Facebook from '../assets/icons/Facebook.png'
+import Instagram from '../assets/icons/instagram.png'
+import Youtube from '../assets/icons/youtube.png'
+import Linkedin from '../assets/icons/linkedin.png'
 
 const Footer = () => {
   return (
-    <div>
-    </div>
+    <footer>
+      <div>
+        <div>
+          <img src={Facebook} alt="" />
+          <img src={Instagram} alt="" />
+          <img src={Youtube} alt="" />
+          <img src={Linkedin} alt="" />
+        </div>
+      </div>
+      
+    </footer>
   )
 }
 
