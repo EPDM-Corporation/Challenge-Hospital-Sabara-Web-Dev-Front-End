5c43760658290143da191b5428b2dba6727ae9fe
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-12 18:15:37 -0300

    Criando os cards de tasks

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -9,9 +9,27 @@ const Home = () => {
     <div>
       <Header/>
       <div id='homeContent'>
-        <h2>Olá André Matos</h2>
-        <div className='taskPanel'>
+        <h2 style={{margin:'25px'}}>Olá André Matos</h2>
+        <div className='taskPanel overflow-auto'>
           <div className='taskPanelTop'></div>
+          {/* <div className='taskCards'>
+            <div className='taskInfo'>
+              <div className='taskTitle'>Entregar pacote para o quarto 205</div>
+              <div className='taskHour'>07/03/2025 13:43</div>
+              <div className='taskDesc'>Entregar o pacote de água e comida para o quarto 205</div>
+              <button className='taskButton'>Finalizar</button>
+            </div>
+            <hr />
+          </div> */}
+           <div className='taskCards'>
+            <div className='taskInfo'>
+              <div className='taskTitle'>Entregar pacote para o quarto 205</div>
+              <div className='taskHour'>07/03/2025 13:43</div>
+              <div className='taskDesc'>Entregar o pacote de água e comida para o quarto 205</div>
+              <button className='taskButton'>Finalizar</button>
+            </div>
+            <hr />
+          </div>
         </div>
       </div>
       <Footer/>
