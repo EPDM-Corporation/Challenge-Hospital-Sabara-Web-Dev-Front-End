1e02fe148a31d50bbc338cc7dc610b619ec5451d
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-12 16:59:12 -0300

    Criando o painel de tasks

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Home.css'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer'
 
@@ -7,7 +8,12 @@ const Home = () => {
   return (
     <div>
       <Header/>
-      <h1>Hello World</h1>
+      <div id='homeContent'>
+        <h2>Olá André Matos</h2>
+        <div className='taskPanel'>
+          <div className='taskPanelTop'></div>
+        </div>
+      </div>
       <Footer/>
     </div>
   )
