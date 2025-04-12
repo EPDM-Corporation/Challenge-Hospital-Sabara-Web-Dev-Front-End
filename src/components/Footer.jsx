8b21c3a577d2f06be02b71170f63166c07bd0a5f
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-12 15:28:02 -0300

    Finalizando o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,12 +8,15 @@ import Linkedin from '../assets/icons/linkedin.png'
 const Footer = () => {
   return (
     <footer>
-      <div>
-        <div>
-          <img src={Facebook} alt="" />
-          <img src={Instagram} alt="" />
-          <img src={Youtube} alt="" />
-          <img src={Linkedin} alt="" />
+      <div id='footerInfo'>
+        <div id='footerSocials'>
+          <img src={Facebook} className='footerIcon' alt="" />
+          <img src={Instagram} className='footerIcon' alt="" />
+          <img src={Youtube} className='footerIcon' alt="" />
+          <img src={Linkedin} className='footerIcon' alt="" />
+        </div>
+        <div id='footerText'>
+        © Projeto de gestão de tarefas  Todos os direitos reservados
         </div>
       </div>
       
