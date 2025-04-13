a834879e14a70c49fcc3a77f8dfa9a7bd6aec6d8
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-04-12 22:47:30 -0300

    Terminando o design do sistema

--- src/routes/System.jsx
+++ src/routes/System.jsx
@@ -6,12 +6,30 @@ import './System.css'
 
 const System = () => {
   return (
-    <div>
+    <div className='systemBody'>
       <header>
         <img src={Logo} id='logo' alt="" />
         <Link to={'/'} id='systemExit'>Sair</Link>
       </header>
+        <section className='systemSection'>
+            <h2 id='systemH2'>Sistema</h2>
+            <form className='systemForms'>
+                <label for="systemTitle">Título:</label>
+                <input type="text" id="systemTitle"></input>
 
+                <label for="systemMedic">Médico:</label>
+                <select id="systemMedic" name="medico">
+                  <option value="andre1">André</option>
+                  <option value="nicolas2">Nicolas</option>
+                </select>
+
+                <label for="systemAction">Serviço/Ação: </label>
+                <input type="text" id="systemAction"></input>
+                <button className='systemButton'>Enviar</button>
+                <button className='systemButton'>Resetar dados</button>
+            </form>
+
+        </section>
         <Footer/>
     </div>
   )
