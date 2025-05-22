2851ba4eeeb63f96abf575e1bad196bdc06e13ba
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-05-22 11:20:05 -0300

    feat: Adicionando o componente Forms do BOOTSTRAP no projeto

--- src/routes/Pacientes.jsx
+++ src/routes/Pacientes.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import Table from '../components/Table/Table'
+import Forms from '../components/Forms/Forms'
 
 const Pacientes = () => {
   return (
     <section>
         <Header/>
+            <Forms></Forms>
             <Table></Table>
         <Footer/>
     </section>
